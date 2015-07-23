7eb7db911f3519165cf3690a27b5f4dbe3073fe0
Andrey Tourkó <tourko@gmail.com>
2015-07-23 13:36:30 +0200

    Enable root login via SSH with empty password

--- archlive/airootfs/root/customize_airootfs.sh
+++ archlive/airootfs/root/customize_airootfs.sh
@@ -12,6 +12,10 @@ chmod 700 /root
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
+# Permit root login via SSH with empty password
+sed -i 's/#\(PermitRootLogin[[:space:]]\+\)no/\1yes/' /etc/ssh/sshd_config
+sed -i 's/#\(PermitEmptyPasswords[[:space:]]\+\)no/\1yes/' /etc/ssh/sshd_config
+
 systemctl enable pacman-init.service choose-mirror.service
 systemctl enable sshd.service
 systemctl set-default multi-user.target
